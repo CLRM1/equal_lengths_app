ced02882833b6d2fea4fd6793a5014851238612e
Chris Romero <chrisleeromero1@gmail.com>
2022-06-26 15:05:34 -0600

    remove duplicate route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  # root 'welcome#index'
   get '/', to: 'welcome#index'
   get '/meditations', to: 'meditations#index'
   get '/meditations/equal_lengths', to: 'equal_lengths#show'
