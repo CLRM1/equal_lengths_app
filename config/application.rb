73be121222deebaef2820da899fd13699dcccc6e
Chris Romero <chrisleeromero1@gmail.com>
2022-06-27 10:54:37 -0600

    revert

--- config/application.rb
+++ config/application.rb
@@ -16,13 +16,13 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.ssl_options = { hsts: { preload: true, expires: 2.year, subdomains: true } }
 
 module EqualLengthsApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     # Force SSL redirect
+    config.ssl_options = { hsts: { preload: true, expires: 1.year, subdomains: true } }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
