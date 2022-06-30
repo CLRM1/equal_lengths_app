47a03b327ee8da00011572fb5f1bda3c5f9f8e66
Chris Romero <chrisleeromero1@gmail.com>
2022-06-30 14:30:21 -0600

    update lading page to meditations index and update format of equal breaths

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,8 @@ module EqualLengthsApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     # Force SSL redirect
-    config.force_ssl = true
-    config.ssl_options = { hsts: { preload: true, expires: 1.year, subdomains: true }, redirect: { status: 307, port: 81 } }
+    # config.force_ssl = true
+    # config.ssl_options = { hsts: { preload: true, expires: 1.year, subdomains: true }, redirect: { status: 307, port: 81 } }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
