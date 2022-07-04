299cd7db8d28561bb7d2dd85dd2dd5dce081c046
Chris Romero <chrisleeromero1@gmail.com>
2022-07-04 11:10:40 -0600

    m

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  match "(*any)", to: redirect("https://www.micromeditations.org/"), via: [:get]
+  match "(*any)", to: redirect_to("https://www.micromeditations.org/"), via: [:get]
   root 'meditations#index'
   get '/meditations', to: 'meditations#index'
   get '/meditations/equal_lengths', to: 'equal_lengths#show'
