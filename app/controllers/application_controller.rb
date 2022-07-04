d40a2c16a736f6e6d8219e7c927a2c923273c6da
Chris Romero <chrisleeromero1@gmail.com>
2022-07-04 10:56:02 -0600

    m

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :add_www_subdomain
+  before_filter :add_www_subdomain
 
   private
   def add_www_subdomain
     unless /^www/.match(request.host)
-      redirect_to("#{request.protocol}micromeditations.com#{request.request_uri}",
+      redirect_to("#{request.protocol}micromeditations.org#{request.request_uri}",
                   :status => 301)
     end
   end
