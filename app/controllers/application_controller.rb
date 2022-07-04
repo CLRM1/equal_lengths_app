2b917c2fd1f079616ab958bc1492a2e0a4603f50
Chris Romero <chrisleeromero1@gmail.com>
2022-07-04 11:40:43 -0600

    m

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :add_www_subdomain
+ before_action :redirect_to_https
+  
+  # before_action :add_www_subdomain
 
-  private
-  def add_www_subdomain
-    unless /^www/.match(request.host)
-      redirect_to("#{request.protocol}micromeditations.org#{request.request_uri}",
-                  :status => 301)
-    end
-  end
+  # private
+  # def add_www_subdomain
+  #   unless /^www/.match(request.host)
+  #     redirect_to("#{request.protocol}micromeditations.org#{request.request_uri}",
+  #                 :status => 301)
+  #   end
+  # end
 end
