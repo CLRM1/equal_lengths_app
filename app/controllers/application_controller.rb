c252808ac16bbb828ead23378103d1828807772e
Chris Romero <chrisleeromero1@gmail.com>
2022-07-04 10:50:28 -0600

    m

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  force_ssl :host => "https://www.micromeditations.org"
+  before_filter :add_www_subdomain
+
+  private
+  def add_www_subdomain
+    unless /^www/.match(request.host)
+      redirect_to("#{request.protocol}micromeditations.com#{request.request_uri}",
+                  :status => 301)
+    end
+  end
 end
