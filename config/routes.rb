af4741b04807fdf9542fd57192e1c9df1effc84a
Chris Romero <chrisleeromero1@gmail.com>
2022-07-04 10:17:17 -0600

    d

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  match "*path" => redirect("https://www.micromeditations.org/%{path}"), :constraints => { :protocol => "http://" }
-  match "*path" => redirect("https://www.micromeditations.org/%{path}"), :constraints => { :subdomain => "" }
+  match "*path" => redirect("https://www.micromeditations.org/%{path}"), via: [:get], :constraints => { :protocol => "http://" }
+  match "*path" => redirect("https://www.micromeditations.org/%{path}"), via: [:get],  :constraints => { :subdomain => "" }
   root 'meditations#index'
   get '/meditations', to: 'meditations#index'
   get '/meditations/equal_lengths', to: 'equal_lengths#show'
