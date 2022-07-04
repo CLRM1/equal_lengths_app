6a324ebb0d55ff22aa3d99a4d42919cce22a937e
Chris Romero <chrisleeromero1@gmail.com>
2022-07-04 09:51:32 -0600

    add route without http

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get '/meditations/square_breathing/restart', to: 'square_breathing#restart'
   get 'restart', to: 'restart#restart'
   get 'http://micromeditations.org', to: 'meditations#index'
+  get 'micromeditations.org', to: 'meditations#index'
 end
