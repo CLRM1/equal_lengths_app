39da560eada8ae8b4636f3a729b66d8c0ac73c04
Chris Romero <chrisleeromero1@gmail.com>
2022-07-06 10:24:22 -0600

    Revert "add ssl config"
    This reverts commit e40b5af63bf8b5ba37278b52bc317e0b4f96c2f3.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,3 @@
 class ApplicationController < ActionController::Base
- before_action :redirect_to_https
-  
-  # before_action :add_www_subdomain
 
-  private
-  def redirect_to_https
-    redirect_to :protocol => "https://www." unless (request.ssl? || request.local?)
-  end
-  # private
-  # def add_www_subdomain
-  #   unless /^www/.match(request.host)
-  #     redirect_to("#{request.protocol}micromeditations.org#{request.request_uri}",
-  #                 :status => 301)
-  #   end
-  # end
 end
--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module EqualLengthsApp
     config.load_defaults 5.2
     # Force SSL redirect
     config.force_ssl = true
-    config.ssl_options = { hsts: { preload: true, expires: 1.year, subdomains: true }, redirect: { host: "micromeditations.org" } }
+    config.ssl_options = { hsts: { preload: true, expires: 1.year, subdomains: true }, redirect: { status: 307, port: 81 } }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
